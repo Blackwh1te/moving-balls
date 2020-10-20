3fbd9b2408a8bdd6d7faf4e85cab0c43a4785fdd
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2020-10-20 14:44:48 +0500

    v1.02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function ($) {
 $(document).ready(function() {
 
+
    let balls = [];
 
    $('.field .ball').each(function () {
@@ -29,17 +30,6 @@ $(document).ready(function() {
       );
    };
 
-
-   function distaceToElementBorder(e, el) {
-      return distanceToElementCenter(e, ball1) - (ball1.width() / 2);
-   };
-   
-   function translate(e, el, distance) {
-      let offset = 1 - (distance * 0.2);
-      // el.css('transition', `all ${0.3}s ease`)
-      el.css('transform', `translate(${offset}px, ${offset}px)`)
-   }
-
    // Distance between element and cursor:
    function posX(e, el) {
       return Math.round( (el.offset().left + el.width() / 2) );
@@ -50,29 +40,7 @@ $(document).ready(function() {
       return Math.round( (el.offset().top + el.height() / 2) );
    };
 
-   // // Перемещение мыши в области расположения шаров:
-   // $('.field').mousemove(function (e) {
-
-   //    console.log( `ball1: ${ distanceToElementCenter(e, ball1) }` );
-   //    // console.log( `ball1 radius: ${ ball1Radius }` );
-
-   //    // console.log( `ball2: ${ distanceToElementCenter(e, ball2) }` );
-   //    // console.log( `ball2 radius: ${ ball2Radius }` );
-
-   //    // console.log( `ball3: ${ distanceToElementCenter(e, ball3) }` );
-   //    // console.log( `ball3 radius: ${ ball3Radius }` );
-
-   //    // if (distanceToElementCenter(e, ball1) < 220) {
-   //    // // ball1.cs
-   //    // translate(e, ball1, distanceToElementCenter(e, ball1));
-   //    // } else {
-   //    // ball1.css('transform', 'translate(0px, 0px)');
-   //    // }
-   // });
-
    function moveTheBall(e, el) {
-      // el.css('transform', `translate(${  }, ${  })`);
-
       let offsetFactor = 0.15;
 
       // Правый верхний угол:
@@ -95,53 +63,15 @@ $(document).ready(function() {
 
    function moveToStart(el) {
      el.css('transform', 'initial'); 
-   };
-   
+   };   
 
-   let timesPerSecond = 15; // how many times to fire the event per second
+   let timesPerSecond = 25; // how many times to fire the event per second
    let wait = false;
    // Отслеживание позиции курсора мыши в области расположения шаров:
    $('.field').mousemove(function (e) {
       // don't handle events when one has just occurred
       if (!wait) {
          
-         // let fieldX = e.pageX;
-         // let fieldY = e.pageY;
-         // console.log('****************');
-         // console.log('fieldX: ' + fieldX);
-         // console.log('fieldY: ' + fieldY);
-         // console.log( `ball1: posX = ${ posX(e, ball1) }` );
-         // console.log( `ball1: posY = ${ posY(e, ball1) }` );
-         // console.log('****************');
-
-         // console.log( `ball2: posX = ${ posX(e, ball2) }` );
-         // console.log( `ball2: posY = ${ posY(e, ball2) }` );
-
-         // console.log( `ball3: posX = ${ posX(e, ball3) }` );
-         // console.log( `ball3: posY = ${ posY(e, ball3) }` );
-
-         // console.log( `Distance to ball1: ${ distanceToElementCenter(e, ball1) }` );
-         // console.log( `Distance to ball2: ${ distanceToElementCenter(e, ball2) }` );
-         // console.log( `Distance to ball3: ${ distanceToElementCenter(e, ball3) }` );
-
-
-
-         // console.log( `Radius ball1: ${ ball1Radius }` );
-         // console.log( `Radius ball2: ${ ball2.width() / 2 }` );
-         // console.log( `Radius ball3: ${ ball3.width() / 2 }` );
-
-         // console.log(`D-R: ${ distaceToElementBorder(e, ball1) }`)
-         // console.log(`D-R: ${ distanceToElementCenter(e, ball2) - (ball2.width() / 2) }`)
-         // console.log(`D-R: ${ distanceToElementCenter(e, ball3) - (ball3.width() / 2) }`)
-         // if (distanceToElementCenter(e, ball2) - ball2.width())
-
-
-         // if ( (distaceToElementBorder(e, ball1) < 100) && (distaceToElementBorder(e, ball1) > 50) ) {
-         //    // console.log('So close...');
-         //    moveTheBall(ball1);
-         // };
-
-
          if ( (distanceToElementCenter(e, ball1) - ball1Radius) < 180 ) {
             moveTheBall(e, ball1);
          } else {
@@ -160,29 +90,6 @@ $(document).ready(function() {
             moveToStart(ball3);
          };
 
-
-
-
-         // console.log( `ball1 radius: ${ ball1Radius }` );   
-         // console.log( `ball2 radius: ${ ball2Radius }` );   
-         // console.log( `ball3 radius: ${ ball3Radius }` );
-   
-         // if (distanceToElementCenter(e, ball1) < 220) {
-         // // ball1.cs
-         // translate(e, ball1, distanceToElementCenter(e, ball1));
-         // } else {
-         // ball1.css('transform', 'translate(0px, 0px)');
-         // }
-
-
-
-
-
-
-
-
-
-
          // stop any further events
          wait = true;
          // after a fraction of a second, allow events again
@@ -194,9 +101,8 @@ $(document).ready(function() {
       moveToStart(ball1);
       moveToStart(ball2);
       moveToStart(ball3);
-   });
+   }); 
 
-   
 
 
 
